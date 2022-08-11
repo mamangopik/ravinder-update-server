c0903ec36e3412c5e80fefc40498e6b16d002a35
Wildan Adition <wildanadition@gmail.com>
2022-08-11 10:46:18 +0000

    Update v1.1-update.py

--- public/v1.1-update.py
+++ public/v1.1-update.py
@@ -5,9 +5,12 @@ import sys
 
 
 try:
-    subprocess.call(["resize2fs", "/dev/mmcblk0p2"])
+    subprocess.call(["raspi-config ", "--expand-rootfs"])
     with open("/home/ravinder/log/update.log", "a+") as file:
         file.write("update to v1.1 success")
+    with open("/home/ravinder-base/version", "w+") as f:
+        f.write("1.1")
+    subprocess.call(['shutdown', '-r', 'now'])
     sys.exit(0)
 except Exception as e:
     with open("/home/ravinder/log/update.log", "a+") as file:
