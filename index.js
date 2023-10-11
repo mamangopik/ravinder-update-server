bc715f2e8b3952466a1c0ccc5700a9bab03afa38
mamangopik <hidayat.12.software@gmail.com>
2023-10-11 17:35:34 +0700

    add version 3

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ app.use('/files', express.static('public'))
 
 app.get('/', (req, res) => {
   res.send('Ravinder Update Server')
+  console.log("accessed")
 })
 app.get('/ravinder-update-path', (req, res) => {
     res.json({"script_link": version.script_link})
+    console.log("accessed upate path")
 })
 app.get('/ravinder-latest-version', (req, res) => {
     res.json({"ravinder": {"version": version.version}})
+    console.log("accessed check version")
 })
 
 app.listen(port, () => {
