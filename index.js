af24f84abf95337488fdeb63da562d08c9b69afa
Taufik Hidayat <145950803+mamangopik@users.noreply.github.com>
2023-10-13 09:58:14 +0700

    Update index.js
    change port number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const version = require('./v3.0/version.json')
-const port = 6500
+const port = 6501
 
 
 app.use('/files', express.static('public'))
