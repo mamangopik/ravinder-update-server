aa390aba42588c6ed277f7cbd2c59e44fa14e605
mamangopik <hidayat.12.software@gmail.com>
2023-10-23 10:08:26 +0700

    modify reconnect-modem.py

--- public/v3.0/v3.0_temp/script/reconnect-modem.py
+++ public/v3.0/v3.0_temp/script/reconnect-modem.py
@@ -6,11 +6,15 @@ from logging import Formatter
 import subprocess
 import time
 from subprocess import call
+import subprocess
 from subprocess import DEVNULL
-from gpiozero import Buzzer
 import schedule
 import sys
 import dotenv
+
+
+# Define the list of network interfaces to test
+test_server = "8.8.8.8"
 
 LOG_FORMAT = "%(asctime)s %(levelname)s %(message)s:%(lineno)d"
 MODEM_LOG_FILE = '/home/ravinder/log/modem.log'
@@ -25,11 +29,10 @@ modem_logger.addHandler(modem_logger_file)
 server_config_file = dotenv.find_dotenv('/etc/config/server.conf')
 current_server_config = dotenv.dotenv_values(server_config_file)
 
-
-def ping():
-    pingRc = call(['ping', '-I', 'wwan0', '-c1', '-w10', '-s0', '8.8.8.8'], stdout=DEVNULL, stderr=DEVNULL)
-    pingServer = call(['ping', '-c1', '-w10', '-s0', current_server_config["ADSB_SBS1_HOST"]], stdout=DEVNULL, stderr=DEVNULL)
-    return pingRc, pingServer
+try:
+    from gpiozero import Buzzer
+except Exception as e:
+    modem_logger.error(str(e))
 
 
 def resetModemPwr():
@@ -40,69 +43,132 @@ def resetModemPwr():
     modemRst.off()
     time.sleep(10)
 
+def get_ping_metrics(interface):
+    ping_result = subprocess.call(
+        ["ping", "-I", interface, "-c", "4", "-q", test_server]
+    )
+    ping_result=str(ping_result)
+    # print("Result",ping_result)
+    RC = int(ping_result.split('\n')[-1])
+    try:
+        return RC
+    except subprocess.CalledProcessError as e:
+        # Handle errors, e.g., if ping fails
+        return 2
+
 
 def restart_service():
     modem_logger.info("restart service")
+    time.sleep(10)
     sys.exit()
 
-
-#schedule.every(5).minutes.do(restart_service)
-
-
-def main():
-    
-    failCount = 0
-    afterDown = False
+def init_modem():
     try:
+        # subprocess.Popen("start-modem.sh", shell=True)
         call(['start-modem.sh'])
         modem_logger.info("Init Modem")
     except:
         modem_logger.error("Failed Init Modem")
-        modem_logger
+
+def soft_reset_modem():
+    status = [0,0]
+    modem_logger.info('Soft Reset Modem')
+    try:
+        process=subprocess.Popen(["stop-modem.sh"], shell=False)
+        time.sleep(60)
+        if process.poll() is None:
+            process.kill()
+            modem_logger.warning('stop modem run too long,process terminated')
+            modem_logger.error('Failed Stop Modem')
+            status[0]=0
+        else:
+            modem_logger.info('stop modem')
+            status[0]=1
+
+        # call(['start-modem.sh'])
+    except:
+        modem_logger.error('Failed Stop Modem')
+        status[0]=0
 
     time.sleep(3)
 
-    while True:
-        rc = ping()
-        if(rc[1]):
-            #print(rc[1])
-            dotenv.set_key(server_config_file, "HAS_CONNECTION",
-                           "False", quote_mode="never")
+    try:
+        process=subprocess.Popen(["start-modem.sh"], shell=False)
+        time.sleep(90)
+        if process.poll() is None:
+            process.kill()
+            modem_logger.warning('start modem run too long,process terminated')
+            modem_logger.error('Failed start Modem')
+            status[1]=0
         else:
-            dotenv.set_key(server_config_file, "HAS_CONNECTION",
-                "True", quote_mode="never")
+            modem_logger.info('Start Modem success')
+            status[1]=1
 
-        # if ping no response success after 10s
-        if rc[0]:
-            #print(rc)
-            if failCount < 7:
-                failCount += 1
-                modem_logger.info('Connection Down')
-                try:
-                    call(['stop-modem.sh'])
-                    modem_logger.info('Soft Reset Modem')
-                    if not (failCount % 3):
-                        resetModemPwr()
-                except:
-                    modem_logger.error('Failed Stop Modem')
+        # call(['start-modem.sh'])
+        time.sleep(5)
+    except:
+        modem_logger.error('Failed start Modem')
+        status[1]=0
 
-                time.sleep(3)
-                try:
-                    call(['start-modem.sh'])
-                    modem_logger.info('Start Modem')
-                    time.sleep(5)
-                    # continue
-                except:
-                    modem_logger.error('Failed Stop Modem')
+    if status[0]==1 and status[1]==1:
+        modem_logger.info('restart modem success')
+    elif status[0]==1 and status[1]==0:
+        modem_logger.error('restart modem failed and modem stoped now')
+    else:
+        modem_logger.error('restart modem failed')
+
+def ping_ravinder_server():
+    ping_result = subprocess.call(
+        ["ping", "-c", "1", "-q", current_server_config["ADSB_SBS1_HOST"]]
+    )
+    ping_result=str(ping_result)
+    # print("Result",ping_result)
+    RC = int(ping_result.split('\n')[-1])
+    try:
+        return RC
+    except subprocess.CalledProcessError as e:
+        # Handle errors, e.g., if ping fails
+        return 1
+
+def main():
+    init_modem()
+    time.sleep(5)
+    no_connection_cnt=0
+    soft_reset_cnt = 0
+    while 1:
+        try:
+            ping_server = ping_ravinder_server()
+            if ping_server == 1:
+                dotenv.set_key(server_config_file, "HAS_CONNECTION","False", quote_mode="never")
             else:
-                #schedule.run_pending()
-                pass
-        # ping succes
-        else:
-            failCount = 0
+                dotenv.set_key(server_config_file, "HAS_CONNECTION","True", quote_mode="never")
+        except:
+            pass
 
-            # wait 10s before check connection again
-        time.sleep(10)
+        RC = get_ping_metrics("wwan0")
+        # print("RC=",RC)
+        if RC == 0:
+            no_connection_cnt=0
+            soft_reset_cnt = 0
+            # modem_logger.info('got connection')
+        elif RC == 1:
+            no_connection_cnt +=1
+        else:
+            modem_logger.info('restart modem service')
+            sys.exit()
+        # jika 5x ping tidak ada koneksi internet
+        if no_connection_cnt%5==0 and no_connection_cnt>0:
+            # print("soft reset modem")
+            soft_reset_modem()
+            soft_reset_cnt += 1
+            if soft_reset_cnt%3==0 and soft_reset_cnt>0:
+                try:
+                    resetModemPwr()
+                    modem_logger.info('hard reset modem OK')
+                    time.sleep(2)
+                except:
+                    modem_logger.error('failed to hard reset modem')
+        time.sleep(1)
 
 
 if __name__ == "__main__":
